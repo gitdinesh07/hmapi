7d97f682c47458a448ddda04d505d8c218a33077
Dinesh Yadav <baidh.dinesh@gmail.com>
2024-05-21 22:43:49 +0530

    first  test commit

---
+++ src/main/java/com/example/hmapi/HmapiApplication.java
@@ -0,0 +1,14 @@
+package com.example.hmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmapiApplication.class, args);
+
+	}
+
+}
