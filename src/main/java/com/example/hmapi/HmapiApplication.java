27c6486ec9875a767638d84d2d61f6a0c3edddc1
Dinesh Yadav <baidh.dinesh@gmail.com>
2024-05-27 09:42:15 +0530

    log4j2 logger implemented

--- src/main/java/com/example/hmapi/HmapiApplication.java
+++ src/main/java/com/example/hmapi/HmapiApplication.java
@@ -1,5 +1,7 @@
 package com.example.hmapi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,12 @@ public class HmapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HmapiApplication.class, args);
 
+		Logger _logger = LogManager.getLogger(HmapiApplication.class);
+		_logger.debug("Debugging log");
+		_logger.info("Info log");
+		_logger.warn("Hey, This is a warning!");
+		_logger.error("Oops! We have an Error. OK");
+		_logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
